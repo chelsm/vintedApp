b0711051c0065b7b3fec801918ad5780cb237c4c
willekenselise <willekens.elise@orange.fr>
2023-06-14 11:45:35 +0200

    test api call

--- products/repositories/products-repository/ProductRepository.tsx
+++ products/repositories/products-repository/ProductRepository.tsx
@@ -2,14 +2,24 @@ import db from "../../config/index";
 import ProductModel from "./ProductRepositoryModel";
 
 export const getAllProducts = async () => {
+  const id = 1;
+  const products = await fetch(`http://localhost:3000/users/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.log(error));
+
+  console.log(products, "hello");
+
   return new Promise((resolve, reject) => {
     db.query("SELECT * FROM products", (error, results) => {
-        if (error) {
-            console.log("Erreur:", error);
-            reject(error);
-          } else {
-            resolve(results); 
-          }
+      if (error) {
+        console.log("Erreur:", error);
+        reject(error);
+      } else {
+        resolve(results);
+      }
     });
   });
 };
@@ -25,7 +35,7 @@ export const getProductById = async (id: number) => {
       }
     });
   });
-}
+};
 
 export const createProduct = async (product: ProductModel) => {
   return new Promise((resolve, reject) => {
@@ -38,22 +48,26 @@ export const createProduct = async (product: ProductModel) => {
       }
     });
   });
-}
+};
 
-export const modifyProductById = async (id:number, product: ProductModel) => {
+export const modifyProductById = async (id: number, product: ProductModel) => {
   return new Promise((resolve, reject) => {
-    db.query("UPDATE products SET ? WHERE id = ?", [product, id], (error, results) => {
-      if (error) {
-        console.log("Erreur:", error);
-        reject(error);
-      } else {
-        resolve(results);
+    db.query(
+      "UPDATE products SET ? WHERE id = ?",
+      [product, id],
+      (error, results) => {
+        if (error) {
+          console.log("Erreur:", error);
+          reject(error);
+        } else {
+          resolve(results);
+        }
       }
-    });
+    );
   });
-}
+};
 
-export const removeProductById = async (id:number) => {
+export const removeProductById = async (id: number) => {
   return new Promise((resolve, reject) => {
     db.query("DELETE FROM products WHERE id = ?", [id], (error, results) => {
       if (error) {
@@ -64,4 +78,4 @@ export const removeProductById = async (id:number) => {
       }
     });
   });
-}
+};
