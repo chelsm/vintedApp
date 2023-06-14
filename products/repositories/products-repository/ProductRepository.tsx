9fc872b2d28f1f0749caeb0be3e7ae1097b1a5a6
willekenselise <willekens.elise@orange.fr>
2023-06-14 15:36:02 +0200

    add catch error

--- products/repositories/products-repository/ProductRepository.tsx
+++ products/repositories/products-repository/ProductRepository.tsx
@@ -3,42 +3,36 @@ import db from "../../config/index";
 import ProductModel from "./ProductRepositoryModel";
 
 export const getAllProducts = async () => {
-
-  /*const id = 1;
-  axios.get(`http://localhost:3000/users/${id}`)
-  .then(function (response) {
-    // handle success
-    console.log(response.data, "response");
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });*/
-
   return new Promise((resolve, reject) => {
     db.query("SELECT * FROM products", (error, results) => {
-      if (error) {
-        console.log("Erreur:", error);
-        reject(error);
-      } else {
-        resolve(results);
+      if (results.length == 0){
+        reject({'message': 'Products not found', 'status': 404});
+      }
+      else{
+        let res = JSON.parse(JSON.stringify(results));
+        resolve({'message': 'Products found', 'data': res, 'status': 200});
       }
     });
+  }).catch((error) => {
+    return error;
   });
 };
 
 export const getProductById = async (id: number) => {
   return new Promise((resolve, reject) => {
-    db.query("SELECT * FROM products WHERE id = ?", [id], (error, results) => {
-      if (error) {
-        console.log("Erreur:", error);
-        reject(error);
-      } else {
-        resolve(results);
+    db.query("SELECT * FROM products WHERE id = ?", [id], ( error, results) => {
+      if (results.length == 0){
+        reject({'message': 'Product not found', 'status': 404});
+      }
+      else{
+        let res = JSON.parse(JSON.stringify(results));
+        resolve({'message': 'Product found', 'data': res, 'status': 200});
       }
     });
+  }).catch((error) => {
+    return error;
   });
-};
+}
 
 export const createProduct = async (product: ProductModel) => {
   return new Promise((resolve, reject) => {
--- users/repositories/users-repository/UserRepository.tsx
+++ users/repositories/users-repository/UserRepository.tsx
@@ -5,29 +5,32 @@ import UserModel from "./UserRepositoryModel";
 export const getAllUsers = async () => {
   return new Promise((resolve, reject) => {
     db.query("SELECT * FROM users", (error, results) => {
-        if (error) {
-            console.log("Erreur:", error);
-            reject(error);
-          } else {
-            resolve(results);
-          }
+      if (results.length == 0) {
+        reject({ message: "Users not found", status: 404 });
+      } else {
+        let res = JSON.parse(JSON.stringify(results));
+        resolve({ message: "Users found", data: res, status: 200 });
+      }
     });
+  }).catch((error) => {
+    return error;
   });
 };
 
 export const getUserById = async (id: number) => {
   return new Promise((resolve, reject) => {
     db.query("SELECT * FROM users WHERE id = ?", [id], (error, results) => {
-      if (error) {
-        console.log("Erreur:", error);
-        reject(error);
+      if (results.length == 0) {
+        reject({ message: "User not found", status: 404 });
       } else {
-        resolve(results);
+        let res = JSON.parse(JSON.stringify(results));
+        resolve({ message: "User found", data: res, status: 200 });
       }
     });
+  }).catch((error) => {
+    return error;
   });
-}
-
+};
 
 export const createUser = async (user: UserModel) => {
   return new Promise((resolve, reject) => {
@@ -40,22 +43,26 @@ export const createUser = async (user: UserModel) => {
       }
     });
   });
-}
+};
 
-export const modifyUserById = async (id:number, user: UserModel) => {
+export const modifyUserById = async (id: number, user: UserModel) => {
   return new Promise((resolve, reject) => {
-    db.query("UPDATE users SET ? WHERE id = ?", [user, id], (error, results) => {
-      if (error) {
-        console.log("Erreur:", error);
-        reject(error);
-      } else {
-        resolve(results);
+    db.query(
+      "UPDATE users SET ? WHERE id = ?",
+      [user, id],
+      (error, results) => {
+        if (error) {
+          console.log("Erreur:", error);
+          reject(error);
+        } else {
+          resolve(results);
+        }
       }
-    });
+    );
   });
-}
+};
 
-export const removeUserById = async (id:number) => {
+export const removeUserById = async (id: number) => {
   return new Promise((resolve, reject) => {
     db.query("DELETE FROM users WHERE id = ?", [id], (error, results) => {
       if (error) {
@@ -66,4 +73,4 @@ export const removeUserById = async (id:number) => {
       }
     });
   });
-}
+};
